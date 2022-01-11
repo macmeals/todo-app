99a59f67f92c7ab1b9acb06695f21adac8fa6e5b
macmeals <68972494+macmeals@users.noreply.github.com>
2022-01-11 23:48:22 +0900

    5回目

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import "./styles.css";
 
 // ページ読み込み時にDOMを読み込ませる。（これがないとaddEventlistnerのIDが存在しない
-window.addEventListener("DOMContentLoaded", function () {
-  // task登録のボタンの要素へアクセス
-  const todobtn = document.getElementById("todosubmit");
+// window.addEventListener("DOMContentLoaded", function () {
+// task登録のボタンの要素へアクセス
+const todobtn = document.getElementById("todosubmit");
 
-  // task登録のボタンをクリックしたら、テキストボックスの内容をコンソールログへ送る
-  todobtn.addEventListener("click", () => {
-    const todotask = document.getElementById("todotext");
-    const texts = todotask.value;
-    // テキストボックスの内容をコンソールログとして出力
-    console.log(texts);
-  });
+// task登録のボタンをクリックしたら、テキストボックスの内容をコンソールログへ送る
+todobtn.addEventListener("click", () => {
+  const todotask = document.getElementById("todotext");
+  const texts = todotask.value;
+  // テキストボックスの内容をコンソールログとして出力
+  console.log(texts);
 });
+// });
