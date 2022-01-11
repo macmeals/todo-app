069847565f7e7200145288c937a4b96cf9f926d6
macmeals <68972494+macmeals@users.noreply.github.com>
2022-01-11 22:52:33 +0900

    Firstcommit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
-
 // ページ読み込み時にDOMを読み込ませる。（これがないとaddEventlistnerのIDが存在しない
 window.onload = function () {
   // task登録のボタンの要素へアクセス
