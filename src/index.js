78305fd53c7078f744ddbbf69da2117922f5a90d
macmeals <68972494+macmeals@users.noreply.github.com>
2022-01-15 00:57:40 +0900

    0115コミット

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import "./styles.css";
 
-// ページ読み込み時にDOMを読み込ませる。（これがないとaddEventlistnerのIDが存在しない
-// window.addEventListener("DOMContentLoaded", function () {
-// task登録のボタンの要素へアクセス
+// Todo登録のボタン要素へアクセス
 const todobtn = document.getElementById("todosubmit");
+// Todo登録のテキストボックスの内容にアクセス
+const todotask = document.getElementById("todotext");
 
-// task登録のボタンをクリックしたら、テキストボックスの内容をコンソールログへ送る
-todobtn.addEventListener("click", () => {
-  const todotask = document.getElementById("todotext");
+// Todoリスト用要素へのアクセス
+const todoList = document.getElementById("todolist");
+// Todoリスト用の「　liタグ　」を追加
+const listItem = document.createElement("li");
+
+todobtn.addEventListener("click", (evt) => {
+  //  テキストボックスの内容を変数に格納
   const texts = todotask.value;
-  // テキストボックスの内容をコンソールログとして出力
-  console.log(texts);
+
+  const showItem = todoList.appendChild(listItem);
+  showItem.innerHTML = texts;
+
+  // // テキストボックスの内容をコンソールログとして出力
+  // console.log(texts);
+
+  // 「登録」ボタンを押すとテキストボックスの中身を空にする
+  todotask.value = "";
 });
-// });
